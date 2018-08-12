365063c6a7ea543719b94f03d9515871f22fddfc
Tempurturtul <tempurturtul@gmail.com>
2018-08-12 15:34:35 -0400

    Add manual tests

--- projects/feidtm/FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ projects/feidtm/FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -21,8 +21,22 @@ public class UrlValidatorTest extends TestCase {
    
    public void testManualTest()
    {
-//You can use this function to implement your manual testing	   
-	   
+	   //You can use this function to implement your manual testing
+	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+
+	   assertTrue(urlVal.isValid("http://www.google.com"));
+//	   assertTrue(urlVal.isValid("https://www.google.com")); // FAILURE: "java.lang.ExceptionInInitializerError: Regular expressions are missing"
+	   assertTrue(urlVal.isValid("http://google.com"));
+	   assertTrue(urlVal.isValid("http://wikipedia.org"));
+	   assertTrue(urlVal.isValid("http://a.co"));
+	   assertTrue(urlVal.isValid("http://a.co/b/c")); // BUG: Should be valid.
+	   assertTrue(urlVal.isValid("http://a.b.co"));
+//	   assertTrue(urlVal.isValid("ftp://foo.com/file.txt")); // FAILURE: "java.lang.ExceptionInInitializerError: Regular expressions are missing"
+//	   assertTrue(urlVal.isValid("ftp://ftp.foo.com/sub/sub/sub/file.txt")); // FAILURE: "java.lang.ExceptionInInitializerError: Regular expressions are missing"
+
+	   assertTrue(!urlVal.isValid("google.com"));
+	   assertTrue(!urlVal.isValid("google"));
+	   assertTrue(!urlVal.isValid(".com"));
    }
    
    
